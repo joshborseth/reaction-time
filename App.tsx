65ab2d03542a923e044068aed17bf3940ff2b484
Joshua Borseth <joshuaborseth@gmail.com>
2022-09-04 00:18:59 -0700

    game is working

--- App.tsx
+++ App.tsx
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useRef, useState } from "react";
+import { Text, View, Pressable } from "react-native";
 
 export default function App() {
+  const [screen, setScreen] = useState<boolean | null>(null);
+  const [gameIsOver, setGameIsOver] = useState<boolean | null>(null);
+  const [playAgain, setPlayAgain] = useState<boolean | null>(null);
+
+  const [time, setTime] = useState<number>(0);
+  const score = useRef<number>(0);
+  useEffect(() => {
+    setPlayAgain(false);
+    setTime(0);
+    setGameIsOver(false);
+    setScreen(false);
+    let timer: any = null;
+    const timeout = setTimeout(() => {
+      setScreen(true);
+      timer = setInterval(() => {
+        setTime((t) => t + 0.1);
+      }, 100);
+    }, Math.floor(Math.random() * 5 + 5) * 1000);
+
+    return () => {
+      clearTimeout(timeout);
+      clearInterval(timer);
+    };
+  }, [playAgain]);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="h-full">
+      {gameIsOver ? (
+        <Pressable className="h-full w-full flex flex-col justify-center items-center bg-blue-500" onPress={() => setPlayAgain(true)}>
+          <Text className=" text-3xl text-white text-center">{score.current > 0 ? score.current.toFixed(1) : "Please wait for screen to be green"}</Text>
+        </Pressable>
+      ) : (
+        <Pressable
+          onPress={() => {
+            setGameIsOver(true);
+            score.current = time;
+          }}
+          className={`${screen ? "bg-green-500" : "bg-red-500"} h-full w-full flex flex-col justify-center items-center`}
+        >
+          <Text className="text-white text-3xl text-center">Click Screen When Screen Is Green</Text>
+        </Pressable>
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
